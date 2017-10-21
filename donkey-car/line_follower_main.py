95f66eeb1f1a77aa0ffa01208760bc7bbc9eb125
Kwabena W. Agyeman <kwagyeman@live.com>
2017-10-21 15:39:53 -0700

    Better code

--- donkey-car/line_follower_main.py
+++ donkey-car/line_follower_main.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2013-2017 Ibrahim Abdelkader <iabdalkader@openmv.io> & Kwabena W. Agyeman <kwagyeman@openmv.io>
 # This work is licensed under the MIT license, see the file LICENSE for details.
 
+import sensor, image, time, math, pyb
+
 ###########
 # Settings
 ###########
@@ -9,18 +11,17 @@
 COLOR_LINE_FOLLOWING = True # False to use grayscale thresholds, true to use color thresholds.
 COLOR_THRESHOLDS = [(90, 100, -20, 127, 40, 127)] # Yellow Line.
 GRAYSCALE_THRESHOLDS = [(240, 255)] # White Line.
-BINARY_VIEW = True # Helps debugging but costs FPS if on.
+BINARY_VIEW = False # Helps debugging but costs FPS if on.
 DO_NOTHING = False # Just capture frames...
-
+FRAME_SIZE = sensor.QQVGA # Frame size.
+FRAME_REGION = 0.75 # Percentage of the image from the bottom (0 - 1.0).
 MAG_THRESHOLD = 5 # Raise to filter out false detections.
-THETA_AVERAGE_WINDOW_SIZE = 4 # Sliding window of averages of this size.
-RHO_AVERAGE_WINDOW_SIZE = 4 # Sliding window of averages of this size.
 
 # Tweak these values for your robocar.
 THROTTLE_CUT_OFF_ANGLE = 10.0 # Maximum angular distance from 90 before we cut speed [0.0-90.0).
 THROTTLE_CUT_OFF_RATE = 0.5 # How much to cut our speed boost (below) once the above is passed (0.0-1.0].
-THROTTLE_GAIN = 3.0 # e.g. how much to speed up on a straight away
-THROTTLE_OFFSET = 25.0 # e.g. default speed
+THROTTLE_GAIN = 0.0 # e.g. how much to speed up on a straight away
+THROTTLE_OFFSET = 24.0 # e.g. default speed
 THROTTLE_P_GAIN = 1.0
 THROTTLE_I_GAIN = 0.0
 THROTTLE_I_MIN = -0.0
@@ -28,17 +29,13 @@ THROTTLE_I_MAX = 0.0
 THROTTLE_D_GAIN = 0.0
 
 # Tweak these values for your robocar.
-STEERING_BOOST_ENABLE = True # Enable the steering boost.s
-STEERING_BOOST_GAIN = 25.0 # How much to add into steering when boosting.
-STEERING_BOOST_ANGLE = 50.0 # Once we need to turn more than this then boost our turn output [0.0-90.0).
-STEERING_BOOST_RATE = 0.15 # How much to boost our turn by (percentage) (0.0-1.0].
-STEERING_THETA_GAIN = 15.0
-STEERING_RHO_GAIN = -1.0
+STEERING_THETA_GAIN = 30.0
+STEERING_RHO_GAIN = -30.0
 STEERING_P_GAIN = 0.6
 STEERING_I_GAIN = 0.0
 STEERING_I_MIN = -0.0
 STEERING_I_MAX = 0.0
-STEERING_D_GAIN = 0.1
+STEERING_D_GAIN = 0.2
 
 # Selects servo controller module...
 ARDUINO_SERVO_CONTROLLER_ATTACHED = True
@@ -55,16 +52,8 @@ STEERING_SERVO_MAX_US = 2300
 # Setup
 ###########
 
-import sensor, image, time, math, pyb
-
-THETA_AVERAGE_WINDOW_SIZE = max(THETA_AVERAGE_WINDOW_SIZE, 1)
-RHO_AVERAGE_WINDOW_SIZE = max(RHO_AVERAGE_WINDOW_SIZE, 1)
-
 THROTTLE_CUT_OFF_ANGLE = max(min(THROTTLE_CUT_OFF_ANGLE, 89.99), 0)
 THROTTLE_CUT_OFF_RATE = max(min(THROTTLE_CUT_OFF_RATE, 1.0), 0.01)
-
-STEERING_BOOST_ANGLE = max(min(STEERING_BOOST_ANGLE, 89.99), 0)
-STEERING_BOOST_RATE = max(min(STEERING_BOOST_RATE, 1.0), 0.01)
 
 # Handle if these were reversed...
 tmp = max(THROTTLE_SERVO_MIN_US, THROTTLE_SERVO_MAX_US)
@@ -79,76 +68,25 @@ STEERING_SERVO_MAX_US = tmp
 # This function maps the output of the linear regression function to a driving vector for steering
 # the robocar. See https://openmv.io/blogs/news/linear-regression-line-following for more info.
 
-t_average = []
-r_average = []
-
 def figure_out_my_steering(line, img):
-    global t_average
-    global r_average
 
-    t = line.theta()
-    r = line.rho()
+    # The "slope_response" tries to turn the wheels in the direction of the line.
+    # It does not keep the car on the line but makes the car drive in the same direction.
+    slope_response = math.tan(math.radians(line.theta()))
 
-    # Step 1: Undo negative rho: [theta + 0, -rho] == [theta + 180, +rho]
+    # Rho is computed using the inverse of this code below in the actual OpenMV Cam code.
+    # This formula comes from the Hough line detection formula (see the wikipedia page for more).
+    # Anyway, the output of this calculations below are a point centered vertically in the middle
+    # of the image and to the left or right such that the line goes through it (cx may be off the image).
+    cy = img.height() / 2
+    cx = (line.rho() - (cy * math.sin(math.radians(line.theta())))) / math.cos(math.radians(line.theta()))
 
-    if r < 0:
-        t += 180
-        r = -r
+    # "cx_middle" is now the distance from the center of the line. This is our error method to stay
+    # on the line. "cx_normal" normalizes the error to something like -1/+1 (it will go over this).
+    cx_middle = cx - (img.width() / 2)
+    cx_normal = cx_middle / (img.width() / 2)
 
-    # Step 2: Determine the driving direction for the quadrant theta is in.
-
-    t_reflected = 0
-
-    if t < 45: # Quadrant 1 (The first 90 degrees are split into two 45 degree quadrants)
-        t_reflected = 180 - t
-        #if(t < 10): t_reflected = 180
-
-    elif t < 90: # Quadrant 2 (The first 90 degrees are split into two 45 degree quadrants)
-        t_reflected = t - 180
-
-    elif t < 180: # Quadrant 3 (90 to 179)
-        t_reflected = 180 - t
-
-    else: # Quadrant 4 (270 to 359 degrees - 180 to 269 never happens)
-        t_reflected = t - 45
-        #if(t > 350): t_reflected = 180
-
-    # Step 3: We need two error function outputs to drive the robocar. One that tries to make the
-    # slope of the line zero and another that tries to center the line in the middle of the field
-    # of view. Both of these error outputs will then be mixed togheter using different gains and
-    # feed into a PID controller in the main loop.
-
-    # sin() produces a larger error output as the slope of the line grows...
-    t_result = math.sin(math.radians(t_reflected))
-
-    # Assuming the slope of the line is zero then cos(t) should be one. One multiplied by rho()
-    # gives you the position of the line on the screen. We then subtract where the line should
-    # be (the center) to get an error output.
-    r_result = math.cos(math.radians(t)) *  (r - (img.width() / 2))
-
-    # Sliding window average. Way easier to do in python versus C.
-    t_average.append(t_result)
-    if len(t_average) > THETA_AVERAGE_WINDOW_SIZE: t_average.pop(0)
-    t = sum(t_average) / len(t_average)
-
-    # Sliding window average. Way easier to do in python versus C.
-    r_average.append(r_result)
-    if len(r_average) > RHO_AVERAGE_WINDOW_SIZE: r_average.pop(0)
-    r = sum(r_average) / len(r_average)
-
-    return (t_result * STEERING_THETA_GAIN) + (r_result * STEERING_RHO_GAIN)
-
-# Solve: STEERING_BOOST_RATE = pow(sin(90 +/- STEERING_BOOST_ANGLE), x) for x...
-#        -> sin(90 +/- STEERING_BOOST_ANGLE) = cos(STEERING_BOOST_ANGLE)
-s_power = math.log(STEERING_BOOST_RATE) / math.log(math.cos(math.radians(STEERING_BOOST_ANGLE)))
-
-def figure_out_my_steering_boost(steering): # steering -> [0:180]
-
-    # 1 - pow(sin()) of the steering angle is only zero when driving straight... e.g. steering ~= 90
-    s_result = 1 - math.pow(math.sin(math.radians(max(min(steering, 179.99), 0.0))), s_power)
-
-    if steering < 90: return steering - (s_result * STEERING_BOOST_GAIN)
-    else: return steering + (s_result * STEERING_BOOST_GAIN)
+    return (slope_response * STEERING_THETA_GAIN) + (cx_normal * STEERING_RHO_GAIN)
 
 # Solve: THROTTLE_CUT_OFF_RATE = pow(sin(90 +/- THROTTLE_CUT_OFF_ANGLE), x) for x...
 #        -> sin(90 +/- THROTTLE_CUT_OFF_ANGLE) = cos(THROTTLE_CUT_OFF_ANGLE)
@@ -191,9 +129,11 @@ def set_servos(throttle, steering):
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565 if COLOR_LINE_FOLLOWING else sensor.GRAYSCALE)
-sensor.set_framesize(sensor.QQQVGA)
+sensor.set_framesize(FRAME_SIZE)
 sensor.set_vflip(True)
 sensor.set_hmirror(True)
+sensor.set_windowing((0, int(sensor.height() * (1.0 - FRAME_REGION)), \
+                     sensor.width(), int(sensor.height() * FRAME_REGION)))
 sensor.skip_frames(time = 0)
 if COLOR_LINE_FOLLOWING: sensor.set_auto_gain(False)
 if COLOR_LINE_FOLLOWING: sensor.set_auto_whitebal(False)
@@ -207,7 +147,7 @@ old_time = pyb.millis()
 
 throttle_old_result = None
 throttle_i_output = 0
-throttle_output = 0
+throttle_output = THROTTLE_OFFSET
 
 steering_old_result = None
 steering_i_output = 0
@@ -215,20 +155,17 @@ steering_output = 90
 
 while True:
     clock.tick()
-    img = sensor.snapshot().histeq() if COLOR_LINE_FOLLOWING else sensor.snapshot().histeq()
+    img = sensor.snapshot().histeq()
     if BINARY_VIEW: img = img.binary(COLOR_THRESHOLDS if COLOR_LINE_FOLLOWING else GRAYSCALE_THRESHOLDS)
-    if BINARY_VIEW: img.erode(1, threshold=2)
-    # if BINARY_VIEW: img.dilate(1, threshold=5)
+    if BINARY_VIEW: img.erode(1, threshold = 2)
     if DO_NOTHING: continue
 
     # We call get regression below to get a robust linear regression of the field of view. This returns
-    # a line object which we can use to steer. Note that the ROI is set to the bottom 3/4ths of the
-    # screen because the top quater of the image is most likely not part of the road.
-    line = img.get_regression(([(50, 100, -128, 127, -128, 127)] if BINARY_VIEW else COLOR_THRESHOLDS) \
-           if COLOR_LINE_FOLLOWING else ([(127, 255)] if BINARY_VIEW else GRAYSCALE_THRESHOLDS), \
-           robust = True, roi = (0, sensor.height() // 4, sensor.width(), sensor.height()))
-    print_string = ""
+    # a line object which we can use to steer.
+    line = img.get_regression(([(50, 100, -128, 127, -128, 127)] if BINARY_VIEW else COLOR_THRESHOLDS) if COLOR_LINE_FOLLOWING \
+                              else ([(127, 255)] if BINARY_VIEW else GRAYSCALE_THRESHOLDS), robust = True)
 
+    print_string = ""
     if line and (line.magnitude() >= MAG_THRESHOLD):
         img.draw_line(line.line(), color = (127, 127, 127) if COLOR_LINE_FOLLOWING else 127)
 
@@ -253,8 +190,6 @@ while True:
 
         # Steering goes from [-90,90] but we need to output [0,180] for the servos.
         steering_output = 90 + max(min(round(steering_pid_output), 90), -90)
-        if STEERING_BOOST_ENABLE: steering_output = figure_out_my_steering_boost(steering_output)
-        if STEERING_BOOST_ENABLE: steering_output = max(min(round(steering_output), 180), 0)
 
         #
         # Figure out throttle and do throttle PID
@@ -278,8 +213,7 @@ while True:
             (throttle_output , steering_output, line.theta(), line.rho())
 
     else:
-        print_string = "Line Lost - throttle %d, steering %d" % \
-            (throttle_output , steering_output)
+        print_string = "Line Lost - throttle %d, steering %d" % (throttle_output , steering_output)
 
     set_servos(throttle_output, steering_output)
     print("FPS %f, %s" % (clock.fps(), print_string))
