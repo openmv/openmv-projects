b940ef94fae532a81764aadab867b6a65c7b52a3
Kwabena W. Agyeman <kwagyeman@live.com>
2018-01-20 19:47:43 -0800

    Winning code

--- donkey-car/line_follower_main.py
+++ donkey-car/line_follower_main.py
@@ -9,19 +9,24 @@ import sensor, image, time, math, pyb
 ###########
 
 COLOR_LINE_FOLLOWING = True # False to use grayscale thresholds, true to use color thresholds.
-COLOR_THRESHOLDS = [(90, 100, -20, 127, 40, 127)] # Yellow Line.
+COLOR_THRESHOLDS = [( 85, 100,  -40,  127,   20,  127)] # Yellow Line.
 GRAYSCALE_THRESHOLDS = [(240, 255)] # White Line.
-BINARY_VIEW = False # Helps debugging but costs FPS if on.
+BINARY_VIEW = True # Helps debugging but costs FPS if on.
 DO_NOTHING = False # Just capture frames...
 FRAME_SIZE = sensor.QQVGA # Frame size.
 FRAME_REGION = 0.75 # Percentage of the image from the bottom (0 - 1.0).
-MAG_THRESHOLD = 5 # Raise to filter out false detections.
+FRAME_WIDE = 1.0 # Percentage of the frame width.
+
+AREA_THRESHOLD = 0 # Raise to filter out false detections.
+PIXELS_THRESHOLD = 40 # Raise to filter out false detections.
+MAG_THRESHOLD = 4 # Raise to filter out false detections.
+MIXING_RATE = 0.9 # Percentage of a new line detection to mix into current steering.
 
 # Tweak these values for your robocar.
-THROTTLE_CUT_OFF_ANGLE = 10.0 # Maximum angular distance from 90 before we cut speed [0.0-90.0).
+THROTTLE_CUT_OFF_ANGLE = 1.0 # Maximum angular distance from 90 before we cut speed [0.0-90.0).
 THROTTLE_CUT_OFF_RATE = 0.5 # How much to cut our speed boost (below) once the above is passed (0.0-1.0].
 THROTTLE_GAIN = 0.0 # e.g. how much to speed up on a straight away
-THROTTLE_OFFSET = 24.0 # e.g. default speed
+THROTTLE_OFFSET = 25.0 # e.g. default speed (0 to 100)
 THROTTLE_P_GAIN = 1.0
 THROTTLE_I_GAIN = 0.0
 THROTTLE_I_MIN = -0.0
@@ -29,13 +34,12 @@ THROTTLE_I_MAX = 0.0
 THROTTLE_D_GAIN = 0.0
 
 # Tweak these values for your robocar.
-STEERING_THETA_GAIN = 30.0
-STEERING_RHO_GAIN = -30.0
-STEERING_P_GAIN = 0.6
+STEERING_OFFSET = 90 # Change this if you need to fix an imbalance in your car (0 to 180).
+STEERING_P_GAIN = -23.0 # Make this smaller as you increase your speed and vice versa.
 STEERING_I_GAIN = 0.0
 STEERING_I_MIN = -0.0
 STEERING_I_MAX = 0.0
-STEERING_D_GAIN = 0.2
+STEERING_D_GAIN = -9 # Make this larger as you increase your speed and vice versa.
 
 # Selects servo controller module...
 ARDUINO_SERVO_CONTROLLER_ATTACHED = True
@@ -52,8 +56,15 @@ STEERING_SERVO_MAX_US = 2300
 # Setup
 ###########
 
+FRAME_REGION = max(min(FRAME_REGION, 1.0), 0.0)
+FRAME_WIDE = max(min(FRAME_WIDE, 1.0), 0.0)
+MIXING_RATE = max(min(MIXING_RATE, 1.0), 0.0)
+
 THROTTLE_CUT_OFF_ANGLE = max(min(THROTTLE_CUT_OFF_ANGLE, 89.99), 0)
 THROTTLE_CUT_OFF_RATE = max(min(THROTTLE_CUT_OFF_RATE, 1.0), 0.01)
+
+THROTTLE_OFFSET = max(min(THROTTLE_OFFSET, 100), 0)
+STEERING_OFFSET = max(min(STEERING_OFFSET, 180), 0)
 
 # Handle if these were reversed...
 tmp = max(THROTTLE_SERVO_MIN_US, THROTTLE_SERVO_MAX_US)
@@ -68,11 +79,9 @@ STEERING_SERVO_MAX_US = tmp
 # This function maps the output of the linear regression function to a driving vector for steering
 # the robocar. See https://openmv.io/blogs/news/linear-regression-line-following for more info.
 
+old_cx_normal = None
 def figure_out_my_steering(line, img):
-
-    # The "slope_response" tries to turn the wheels in the direction of the line.
-    # It does not keep the car on the line but makes the car drive in the same direction.
-    slope_response = math.tan(math.radians(line.theta()))
+    global old_cx_normal
 
     # Rho is computed using the inverse of this code below in the actual OpenMV Cam code.
     # This formula comes from the Hough line detection formula (see the wikipedia page for more).
@@ -85,8 +94,16 @@ def figure_out_my_steering(line, img):
     # on the line. "cx_normal" normalizes the error to something like -1/+1 (it will go over this).
     cx_middle = cx - (img.width() / 2)
     cx_normal = cx_middle / (img.width() / 2)
+    # Note that "cx_normal" may be larger than -1/+1. When the value is between -1/+1 this means the
+    # robot is driving basically straight and needs to only turn lightly left or right. When the value
+    # is outside -1/+1 it means you need to turn VERY hard to the left or right to get back on the
+    # line. This maps to the case of the robot driving into a horizontal line. "cx_normal" will
+    # then approach -inf/+inf depending on how horizontal the line is. What's nice is that this
+    # is exactly the behavior we want and it gets up back on the line!
 
-    return (slope_response * STEERING_THETA_GAIN) + (cx_normal * STEERING_RHO_GAIN)
+    if old_cx_normal != None: old_cx_normal = (cx_normal * MIXING_RATE) + (old_cx_normal * (1.0 - MIXING_RATE))
+    else: old_cx_normal = cx_normal
+    return old_cx_normal
 
 # Solve: THROTTLE_CUT_OFF_RATE = pow(sin(90 +/- THROTTLE_CUT_OFF_ANGLE), x) for x...
 #        -> sin(90 +/- THROTTLE_CUT_OFF_ANGLE) = cos(THROTTLE_CUT_OFF_ANGLE)
@@ -132,9 +149,9 @@ sensor.set_pixformat(sensor.RGB565 if COLOR_LINE_FOLLOWING else sensor.GRAYSCALE
 sensor.set_framesize(FRAME_SIZE)
 sensor.set_vflip(True)
 sensor.set_hmirror(True)
-sensor.set_windowing((0, int(sensor.height() * (1.0 - FRAME_REGION)), \
-                     sensor.width(), int(sensor.height() * FRAME_REGION)))
-sensor.skip_frames(time = 0)
+sensor.set_windowing((int((sensor.width() / 2) - ((sensor.width() / 2) * FRAME_WIDE)), int(sensor.height() * (1.0 - FRAME_REGION)), \
+                     int((sensor.width() / 2) + ((sensor.width() / 2) * FRAME_WIDE)), int(sensor.height() * FRAME_REGION)))
+sensor.skip_frames(time = 200)
 if COLOR_LINE_FOLLOWING: sensor.set_auto_gain(False)
 if COLOR_LINE_FOLLOWING: sensor.set_auto_whitebal(False)
 clock = time.clock()
@@ -151,19 +168,22 @@ throttle_output = THROTTLE_OFFSET
 
 steering_old_result = None
 steering_i_output = 0
-steering_output = 90
+steering_output = STEERING_OFFSET
 
 while True:
     clock.tick()
     img = sensor.snapshot().histeq()
+
     if BINARY_VIEW: img = img.binary(COLOR_THRESHOLDS if COLOR_LINE_FOLLOWING else GRAYSCALE_THRESHOLDS)
-    if BINARY_VIEW: img.erode(1, threshold = 2)
+    if BINARY_VIEW: img.erode(1, threshold = 3).dilate(1, threshold = 1)
     if DO_NOTHING: continue
 
-    # We call get regression below to get a robust linear regression of the field of view. This returns
-    # a line object which we can use to steer.
+    # We call get regression below to get a robust linear regression of the field of view.
+    # This returns a line object which we can use to steer the robocar.
     line = img.get_regression(([(50, 100, -128, 127, -128, 127)] if BINARY_VIEW else COLOR_THRESHOLDS) if COLOR_LINE_FOLLOWING \
-                              else ([(127, 255)] if BINARY_VIEW else GRAYSCALE_THRESHOLDS), robust = True)
+        else ([(127, 255)] if BINARY_VIEW else GRAYSCALE_THRESHOLDS), \
+        area_threshold = AREA_THRESHOLD, pixels_threshold = PIXELS_THRESHOLD, \
+        robust = True)
 
     print_string = ""
     if line and (line.magnitude() >= MAG_THRESHOLD):
@@ -189,7 +209,7 @@ while True:
                               (STEERING_D_GAIN * steering_d_output)
 
         # Steering goes from [-90,90] but we need to output [0,180] for the servos.
-        steering_output = 90 + max(min(round(steering_pid_output), 90), -90)
+        steering_output = STEERING_OFFSET + max(min(round(steering_pid_output), 180 - STEERING_OFFSET), STEERING_OFFSET - 180)
 
         #
         # Figure out throttle and do throttle PID
@@ -216,4 +236,4 @@ while True:
         print_string = "Line Lost - throttle %d, steering %d" % (throttle_output , steering_output)
 
     set_servos(throttle_output, steering_output)
-    print("FPS %f, %s" % (clock.fps(), print_string))
+    print("FPS %f - %s" % (clock.fps(), print_string))
